b0301167a2e2167272de775ac9ce90e6b29cd2e9
edsonlb <ti_edson@yahoo.com.br>
2015-08-15 13:58:22 -0300

    Editar

--- pessoa/views.py
+++ pessoa/views.py
@@ -18,3 +18,11 @@ def excluir(request, codigo):
     pessoa = Pessoa.objects.get(pk=codigo)
     pessoa.delete()
     return HttpResponseRedirect('/')
+
+def pesquisa(request):
+    
+    if request.method == 'GET':
+        pessoas = Pessoa.objects.filter(nome__contains=request.GET.get('busca'))
+    
+    return render(request,'index.html',
+        {'msg':'Resultado da Busca','pessoas':pessoas})
